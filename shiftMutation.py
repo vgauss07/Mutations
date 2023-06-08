2fa5758ebeb167e78c466069df7e5d7aef6908b7
vgauss07 <vgauss07@gmail.com>
2023-06-08 13:31:42 -0400

    Update README.md

--- shiftMutation.py
+++ shiftMutation.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 # Shift mutation is moving a gene from a randomly selected position
 # by a random number of positions to the left or right.
-=======
 # Shift Mutation - Bounded Shifting Approach
 
 """
@@ -10,18 +8,13 @@ a random number of positions to the left or right.
 There are several approaches of implementing
 shift mutation: bounded and unbounded
 """
->>>>>>> ec3fb81 (Update README.md)
+
 
 import copy
 import random
 from math import copysign
 
-
-<<<<<<< HEAD
-def mutation_shift(ind):
-=======
 def shiftMutation(ind):
->>>>>>> ec3fb81 (Update README.md)
     mutation = copy.deepcopy(ind)
     position = random.sample(range(0, len(mutation)), 2)
     g1 = mutation[position[0]]
@@ -31,16 +24,9 @@ def shiftMutation(ind):
     mutation[position[1]] = g1
     return mutation
 
-<<<<<<< HEAD
-random.seed(40)
-
-ind = list(range(1,6))
-mutation = mutation_shift(ind)
-=======
 
 random.seed(20)
 ind = list(range(1,6))
-mutation = shiftMutation(ind) 
->>>>>>> ec3fb81 (Update README.md)
+mutation = shiftMutation(ind)
 print(f'Original: {ind}')
 print(f'Mutated: {mutation}')
